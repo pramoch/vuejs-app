245e6be128b16dd5644d23f8b1ad48281b54fcc0
Pramoch Chulapong <pramoch.chulapong@gmail.com>
2020-07-09 14:16:22 +0700

    Add vue-router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Router from 'vue-router'
+import { routes } from './router/index'
+
+Vue.use(Router)
+const router = new Router({ routes })
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,7 @@
+import Home from '../components/Home'
+import User from '../components/User'
+
+export const routes = [
+  { path: '', component: Home },
+  { path: '/user', component: User }
+]
